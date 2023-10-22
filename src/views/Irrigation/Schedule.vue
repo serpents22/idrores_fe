2c7d5013657a40b4b13ff0e7ae34edcd939ea3b0
serpents22 <ivandarmawan67@gmail.com>
2023-10-22 13:45:05 +0700

    fix: responsiveness

--- src/views/Irrigation/Schedule.vue
+++ src/views/Irrigation/Schedule.vue
@@ -15,23 +15,21 @@
   <div class="dashboard-container">
     <IdroTitle :title="title"/>
     <div class="content">
-      <div class="row">
-        <div class="sm-icon-card" v-if="devicesStore.deviceData.role !== 'user'">
-          <router-link  :to="{ name: 'Programma' }" >
-            <img src="@/assets/programma_partenze.png">
-          </router-link>
-          <p>{{ $t('scheduleStart') }}</p>
-        </div>
-        <div class="sm-icon-card opacity-40" v-else>
-          <img src="@/assets/programma_partenze.png" >
-          <p>{{ $t('scheduleStart') }}</p>
-        </div>
-        <div class="sm-icon-card">
-          <router-link :to="{ name: 'Avvio' }">
-            <img src="@/assets/avvio_manuale.png">
-          </router-link>
-          <p>{{ $t('manualStart') }}</p>
-        </div>
+      <div class="sm-icon-card" v-if="devicesStore.deviceData.role !== 'user'">
+        <router-link  :to="{ name: 'Programma' }" >
+          <img src="@/assets/programma_partenze.png">
+        </router-link>
+        <p>{{ $t('scheduleStart') }}</p>
+      </div>
+      <div class="sm-icon-card opacity-40" v-else>
+        <img src="@/assets/programma_partenze.png" >
+        <p>{{ $t('scheduleStart') }}</p>
+      </div>
+      <div class="sm-icon-card">
+        <router-link :to="{ name: 'Avvio' }">
+          <img src="@/assets/avvio_manuale.png">
+        </router-link>
+        <p>{{ $t('manualStart') }}</p>
       </div>
       <div class="row">
         <div class="sm-icon-card" v-if="devicesStore.deviceData.role !== 'user'">
