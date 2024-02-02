4b452098ba5bcef72cf54c359186e5514c01b28e
serpents22 <ivandarmawan67@gmail.com>
2024-02-02 22:35:24 +0700

    feat: change button color

--- src/views/ManageAccount.vue
+++ src/views/ManageAccount.vue
@@ -1,36 +1,20 @@
 <template>
-
   <div class="slider"></div>
   <loading :loading="isLoading" />
 
-  <DeleteAccount
-    :isOpen="isShowDeleteAccount"
-    @close="toggleDeleteAccount" 
-    title="Delete Account"
-  />
-  <ShareDevice
-    :isOpen="isShowAddRole"
-    @close="toggleAddRole" 
-    :title="$t('addRule')"
-  />
-  <ManageShare
-    :isOpen="isShowManageRole"
-    @close="toggleManageRole" 
-    :title="$t('manageRule')" 
-  />
-    
-  <sidebar
-    :noSocial="true" 
-    :backOn="true" 
-    />
+  <DeleteAccount :isOpen="isShowDeleteAccount" @close="toggleDeleteAccount" title="Delete Account" />
+  <ShareDevice :isOpen="isShowAddRole" @close="toggleAddRole" :title="$t('addRule')" />
+  <ManageShare :isOpen="isShowManageRole" @close="toggleManageRole" :title="$t('manageRule')" />
+
+  <sidebar :noSocial="true" :backOn="true" />
   <div class="main-container">
     <IdroTitle :title="title" />
     <div class="manage-account-container">
       <div class="addrole" @click="toggleAddRole">
-        <span>{{$t('addRule')}}</span>
+        <span>{{ $t('addRule') }}</span>
       </div>
       <div class="managerole" @click="toggleManageRole">
-        <span>{{$t('manageRule')}}</span>
+        <span>{{ $t('manageRule') }}</span>
       </div>
       <div class="addrole" @click="toggleDeleteAccount">
         <span>Delete Account</span>
@@ -49,25 +33,25 @@ import { onMounted } from 'vue'
 
 const deviceManagementStore = useDeviceManagement()
 
-  onMounted(() => {
-    deviceManagementStore.superAdminDevices()
-  })
-  //init store
-  const title = 'Gestisci account'
-  //function
-  const isShowDeleteAccount = ref(false)
-  const isShowAddRole = ref(false)
-  const isShowManageRole = ref(false)
+onMounted(() => {
+  deviceManagementStore.superAdminDevices()
+})
+//init store
+const title = 'Gestisci account'
+//function
+const isShowDeleteAccount = ref(false)
+const isShowAddRole = ref(false)
+const isShowManageRole = ref(false)
 
-  function toggleDeleteAccount() {
-    isShowDeleteAccount.value = !isShowDeleteAccount.value
-  }
-  function toggleAddRole() {
-    isShowAddRole.value = !isShowAddRole.value
-  }
-  function toggleManageRole() {
-    isShowManageRole.value = !isShowManageRole.value
-  }
+function toggleDeleteAccount() {
+  isShowDeleteAccount.value = !isShowDeleteAccount.value
+}
+function toggleAddRole() {
+  isShowAddRole.value = !isShowAddRole.value
+}
+function toggleManageRole() {
+  isShowManageRole.value = !isShowManageRole.value
+}
 
 
 
@@ -91,34 +75,39 @@ span p {
 }
 
 .impiantos {
-  @apply  flex flex-wrap justify-between gap-8
-          w-[1000px] mx-auto
+  @apply flex flex-wrap justify-between gap-8 w-[1000px] mx-auto
 }
 
 .impiantos modal {
   @apply w-72 transition ease-in-out delay-150 hover:-translate-y-2
 }
-.addrole, .managerole{
-  background: linear-gradient(45.06deg, #010AD1 -8.2%, #1BF728 108.15%);
-  border: 2px solid #FFEE58;
+
+.addrole, .managerole {
+  background: linear-gradient(45.06deg, #FFFFFF -8.2%, #FFFFFF 108.15%);
+  border: 3px solid #000000;
   padding: 20px;
   @apply text-white font-medium rounded-lg w-60
-    /* border: 1px solid #031ac4;
+  /* border: 1px solid #031ac4;
     width: 25%;
     padding: 50px;
     border-radius: 24px;
     margin: 10px; */
 }
-.manage-account-container{
+
+.manage-account-container {
   @apply gap-10 flex flex-col sm:flex-row justify-center items-center mt-10
 }
-.addrole:hover, .managerole:hover{
-  cursor:pointer;
+
+.addrole:hover, .managerole:hover {
+  cursor: pointer;
 }
-@media (max-width:980px){
-.addrole, .managerole{
+
+@media (max-width:980px) {
+
+  .addrole,
+  .managerole {
     padding: 20px;
-}
+  }
 }
 
 .main-container {
